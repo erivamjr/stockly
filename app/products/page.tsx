2f95a9230741054f178f413657d37c607714cbcb
José Erivam <erivamdev@gmail.com>
2025-04-06 08:01:50 -0300

    :sparkles: feat: add iproduct tabel

--- app/products/page.tsx
+++ app/products/page.tsx
@@ -1,9 +1,29 @@
-const Products = () => {
+import { db } from "../_lib/prisma";
+import { Button } from "../_components/ui/button";
+import { PlusIcon } from "lucide-react";
+import { DataTable } from "../_components/ui/data-table";
+import { productTableColumns } from "./_components/table-columns";
+
+const ProductsPage = async () => {
+  const products = await db.product.findMany({});
   return (
-    <div>
-      <h1>Products</h1>
+    <div className="m-8 w-full space-y-8 rounded-lg bg-white p-8">
+      <div className="flex w-full items-center justify-between">
+        <div className="space-y-1">
+          <span className="text-xs font-semibold text-slate-500">
+            Gestao de Produtos
+          </span>
+          <h2 className="text-xl font-semibold">Produtos</h2>
+        </div>
+        <Button>
+          <PlusIcon size={20} className="gap-2" />
+          Novo produto
+        </Button>
+      </div>
+
+      <DataTable columns={productTableColumns} data={products} />
     </div>
   );
 };
 
-export default Products;
+export default ProductsPage;
