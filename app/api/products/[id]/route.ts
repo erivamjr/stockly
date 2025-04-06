eb4b4308939923f936fba63f3be98e19df9ff7c8
José Erivam <erivamdev@gmail.com>
2025-04-06 14:17:05 -0300

    :recycle: feractor: add router handlers

---
+++ app/api/products/[id]/route.ts
@@ -0,0 +1,19 @@
+import { NextRequest } from "next/server";
+import { db } from "../../../_lib/prisma";
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } },
+) {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("teste") || null;
+  console.log("Query: ", query);
+  const { id } = params;
+  const product = await db.product.findUnique({
+    where: { id: id as string },
+  });
+  if (!product) {
+    return Response.json({ error: "Product not found" }, { status: 404 });
+  }
+  return Response.json(product, { status: 200 });
+}
