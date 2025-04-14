a0a5829a613c996c7acc9cc52c0b24cca8510bc9
José Erivam <erivamdev@gmail.com>
2025-04-14 17:42:05 -0300

    :sparkles: feat: ensure sale creation is done with a transaction

--- app/_actions/sale/create-sale/index.ts
+++ app/_actions/sale/create-sale/index.ts
@@ -6,33 +6,35 @@ import { createSaleSchema, CreateSaleSchema } from "./schema";
 
 export const createSale = async (data: CreateSaleSchema) => {
   createSaleSchema.parse(data);
-  const sale = await db.sale.create({ data: { date: new Date() } });
+  await db.$transaction(async (tx) => {
+    const sale = await tx.sale.create({ data: { date: new Date() } });
 
-  for (const product of data.products) {
-    const productFromDb = await db.product.findUnique({
-      where: { id: product.id },
-    });
-    if (!productFromDb) {
-      throw new Error("Product not found");
+    for (const product of data.products) {
+      const productFromDb = await db.product.findUnique({
+        where: { id: product.id },
+      });
+      if (!productFromDb) {
+        throw new Error("Product not found");
+      }
+
+      const productIsOutOfStock = product.quantity > productFromDb.stock;
+      if (productIsOutOfStock) {
+        throw new Error("Product is out of stock");
+      }
+      await tx.saleProduct.create({
+        data: {
+          saleId: sale.id,
+          productId: product.id,
+          quantity: product.quantity,
+          unitPrice: productFromDb.price,
+        },
+      });
+
+      await tx.product.update({
+        where: { id: product.id },
+        data: { stock: { decrement: product.quantity } },
+      });
     }
-
-    const productIsOutOfStock = product.quantity > productFromDb.stock;
-    if (productIsOutOfStock) {
-      throw new Error("Product is out of stock");
-    }
-    await db.saleProduct.create({
-      data: {
-        saleId: sale.id,
-        productId: product.id,
-        quantity: product.quantity,
-        unitPrice: productFromDb.price,
-      },
-    });
-
-    await db.product.update({
-      where: { id: product.id },
-      data: { stock: { decrement: product.quantity } },
-    });
-  }
-  revalidatePath("/products");
+    revalidatePath("/products");
+  });
 };
