84278eea320e132ddbce5f5f6df967ba409b6c73
José Erivam <erivamdev@gmail.com>
2025-04-19 13:58:12 -0300

    :sparkles: feat: ensure product stock is recovered other sale is deleted

--- app/_actions/sale/delete-sale/index.ts
+++ app/_actions/sale/delete-sale/index.ts
@@ -8,6 +8,24 @@ import { deleteSaleSchema } from "./schema";
 export const deleteSale = actionClient
   .schema(deleteSaleSchema)
   .action(async ({ parsedInput: { id } }) => {
-    await db.sale.delete({ where: { id } });
+    await db.$transaction(async (tx) => {
+      const sales = await tx.sale.findUnique({
+        where: { id },
+        include: { saleProducts: true },
+      });
+
+      if (!sales) return;
+
+      await tx.sale.delete({ where: { id } });
+
+      for (const product of sales.saleProducts) {
+        await tx.product.update({
+          where: { id: product.productId },
+          data: { stock: { increment: product.quantity } },
+        });
+      }
+    });
     revalidatePath("/sales");
+    revalidatePath("/products");
+    revalidatePath("/");
   });
